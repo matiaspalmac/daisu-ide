338ed4d21924f06677ee298be3d625581c10b409
matiaspalmac <matiaspalma2594@gmail.com>
2026-05-02 10:22:22 -0400

    test(ui): add a11y check for ToastViewport

--- apps/ui/tests/setup.ts
+++ apps/ui/tests/setup.ts
@@ -1,1 +1,12 @@
 import "@testing-library/jest-dom/vitest";
+import { expect } from "vitest";
+import * as matchers from "vitest-axe/matchers";
+import type { AxeMatchers } from "vitest-axe/matchers";
+
+expect.extend(matchers);
+
+declare module "vitest" {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  interface Assertion<T = any> extends AxeMatchers {}
+  interface AsymmetricMatchersContaining extends AxeMatchers {}
+}
